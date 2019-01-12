c9f814d445a73e2a0d63773117adb1204c910b0b
Ivan Perun <peruni101@gmail.com>
2019-01-11 23:45:16 -0500

    styling update

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -1,13 +1,21 @@
-var Typer = function(element) {
+var Typer = function (element) {
   this.element = element;
   var delim = element.dataset.delim || ","; // default to comma
   var words = element.dataset.words || "override these,sample typing";
-  this.words = words.split(delim).filter(function(v){return v;}); // non empty words
+  this.words = words.split(delim).filter(function (v) {
+    return v;
+  }); // non empty words
   this.delay = element.dataset.delay || 200;
   this.loop = element.dataset.loop || "true";
   this.deleteDelay = element.dataset.deletedelay || element.dataset.deleteDelay || 800;
 
-  this.progress = { word:0, char:0, building:true, atWordEnd:false, looped: 0 };
+  this.progress = {
+    word: 0,
+    char: 0,
+    building: true,
+    atWordEnd: false,
+    looped: 0
+  };
   this.typing = true;
 
   var colors = element.dataset.colors || "black";
@@ -18,16 +26,16 @@ var Typer = function(element) {
   this.doTyping();
 };
 
-Typer.prototype.start = function() {
+Typer.prototype.start = function () {
   if (!this.typing) {
     this.typing = true;
     this.doTyping();
   }
 };
-Typer.prototype.stop = function() {
+Typer.prototype.stop = function () {
   this.typing = false;
 };
-Typer.prototype.doTyping = function() {
+Typer.prototype.doTyping = function () {
   var e = this.element;
   var p = this.progress;
   var w = p.word;
@@ -39,7 +47,9 @@ Typer.prototype.doTyping = function() {
     this.cursor.on = true;
     clearInterval(this.cursor.interval);
     var itself = this.cursor;
-    this.cursor.interval = setInterval(function() {itself.updateBlinkState();}, 400);
+    this.cursor.interval = setInterval(function () {
+      itself.updateBlinkState();
+    }, 400);
   }
 
   e.innerHTML = currentDisplay;
@@ -62,30 +72,32 @@ Typer.prototype.doTyping = function() {
     }
   }
 
-  if(p.atWordEnd) p.looped += 1;
+  if (p.atWordEnd) p.looped += 1;
 
-  if(!p.building && (this.loop == "false" || this.loop <= p.looped) ){
+  if (!p.building && (this.loop == "false" || this.loop <= p.looped)) {
     this.typing = false;
   }
 
   var myself = this;
-  setTimeout(function() {
-    if (myself.typing) { myself.doTyping(); };
+  setTimeout(function () {
+    if (myself.typing) {
+      myself.doTyping();
+    };
   }, p.atWordEnd ? this.deleteDelay : this.delay);
 };
 
-var Cursor = function(element) {
+var Cursor = function (element) {
   this.element = element;
   this.cursorDisplay = element.dataset.cursordisplay || "_";
   element.innerHTML = this.cursorDisplay;
   this.on = true;
   element.style.transition = "all 0.1s";
   var myself = this;
-  this.interval = setInterval(function() {
+  this.interval = setInterval(function () {
     myself.updateBlinkState();
   }, 400);
 }
-Cursor.prototype.updateBlinkState = function() {
+Cursor.prototype.updateBlinkState = function () {
   if (this.on) {
     this.element.style.opacity = "0";
     this.on = false;
@@ -104,12 +116,16 @@ function TyperSetup() {
   var elements = document.getElementsByClassName("typer-stop");
   for (var i = 0, e; e = elements[i++];) {
     let owner = typers[e.dataset.owner];
-    e.onclick = function(){owner.stop();};
+    e.onclick = function () {
+      owner.stop();
+    };
   }
   var elements = document.getElementsByClassName("typer-start");
   for (var i = 0, e; e = elements[i++];) {
     let owner = typers[e.dataset.owner];
-    e.onclick = function(){owner.start();};
+    e.onclick = function () {
+      owner.start();
+    };
   }
 
   var elements2 = document.getElementsByClassName("cursor");
