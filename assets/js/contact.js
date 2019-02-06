4bc08bba97b8d663b376038818992baff03b7bcb
flex2016 <reloded2004@hotmail.com>
2019-02-06 14:44:30 -0500

    added contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,42 @@
+// Input Lock
+$('textarea').blur(function () {
+  $('#contact textarea').each(function () {
+      $this = $(this);
+      if ( this.value != '' ) {
+        $this.addClass('focused');
+        $('textarea + label + span').css({'opacity': 1});
+      }
+      else {
+        $this.removeClass('focused');
+        $('textarea + label + span').css({'opacity': 0});
+      }
+  });
+});
+
+$('#contact .field:first-child input').blur(function () {
+  $('#contact .field:first-child input').each(function () {
+      $this = $(this);
+      if ( this.value != '' ) {
+        $this.addClass('focused');
+        $('.field:first-child input + label + span').css({'opacity': 1});
+      }
+      else {
+        $this.removeClass('focused');
+        $('.field:first-child input + label + span').css({'opacity': 0});
+      }
+  });
+});
+
+$('#contact .field:nth-child(2) input').blur(function () {
+  $('#contact .field:nth-child(2) input').each(function () {
+      $this = $(this);
+      if ( this.value != '' ) {
+        $this.addClass('focused');
+        $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+      }
+      else {
+        $this.removeClass('focused');
+        $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+      }
+  });
+});
